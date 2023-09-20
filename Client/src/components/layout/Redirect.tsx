4fc6ea2878d4fae0d543a85d8e2274e823dad039
Huai <chrbezz0487@gmail.com>
2023-09-21 01:05:23 +0800

    Client Update

---
+++ Client/src/components/layout/Redirect.tsx
@@ -0,0 +1,15 @@
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { webRoutes } from '../../routes/web';
+import { RootState } from '../../store';
+
+const Redirect = () => {
+  // const admin = useSelector((state: RootState) => state.admin);
+
+  return (
+    // <Navigate to={admin ? webRoutes.dashboard : webRoutes.login} replace />
+    <Navigate to={webRoutes.dashboard } replace />
+    );
+};
+
+export default Redirect;
